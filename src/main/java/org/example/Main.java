254ab25c2b2097e5cfcfb96a0ced4e257f27a91d
kirylfishman <fishmank89@gmail.com>
2023-10-25 20:14:19 +0300

    upd branch mcr

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,9 @@
 package org.example;
-
-
 public class Main {
     public static void main(String[] args) {
-
         System.out.printf("Hello and welcome!");
 System.out.println();
-        System.out.println("mergeconflresolve");
+System.out.println("mergeconflresolve");
 
 
     }
